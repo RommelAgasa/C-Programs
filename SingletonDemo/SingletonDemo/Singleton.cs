f20bcf92346f0c2f3854413c6197af203c140691
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:15:40 +0800

    Add files via upload

---
+++ SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    internal class Singleton
+    {
+        private static Singleton instance = null;
+
+
+        private Singleton()
+        {
+            // Initialization of instance variables
+        }
+
+        public static Singleton Instance()
+        {
+            if(instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+}
