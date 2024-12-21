e0bd8324a851b2a4ad71f655ceaffd20bcfef936
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:13:00 +0800

    Add files via upload

---
+++ Nullable Type Demo/Nullable Type Demo/IntSequence.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable_Type_Demo
+{
+    internal class IntSequence
+    {
+
+        private int[] sequence;
+
+        public IntSequence(params int[] elements)
+        {
+            sequence = new int[elements.Length];
+            for(int i = 0; i < elements.Length; i++)
+            {
+                sequence[i] = elements[i];
+            }
+        }
+
+        public int? Min()
+        {
+            int theMinimum;
+            if (sequence.Length == 0) return null;
+            theMinimum = sequence[0];
+            for(int i = 1; i < sequence.Length; i++)
+                if(sequence[i] < theMinimum)
+                    theMinimum = sequence[i];
+            return theMinimum;
+        }
+
+        public int? Max()
+        {
+            int theMaximum;
+            if (sequence.Length == 0) return null;
+            theMaximum = sequence[0];
+            for (int i = 1; i < sequence.Length; i++)
+                if (sequence[i] < theMaximum)
+                    theMaximum = sequence[i];
+            return theMaximum;
+
+        }
+    }
+}
---
+++ Nullable Type Demo/Nullable Type Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Nullable_Type_Demo
+{
+
+    public struct a
+    {
+        public int Value;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntSequence is1 = new IntSequence(-5, -1, 7, -8, 13),
+                is2 = new IntSequence();
+
+            int? x = null;
+
+            ReportMinMax(is1);
+            ReportMinMax(is2);
+
+            //a a = new a();
+            //a.Value = 5;
+            //Console.WriteLine(a);
+
+        }
+
+        // The property HasValue can be applied 
+        // on a value of a nullable type.
+        public static void ReportMinMax(IntSequence iseq)
+        {
+            if(iseq.Min().HasValue && iseq.Max().HasValue)
+                Console.WriteLine("Min: {0}. Max: {1}",
+                            iseq.Min(), iseq.Max());
+            else
+                Console.WriteLine("Int sequence is empty");
+        }
+    }
+}   
