086f9cfa4360669e68e5ddf9a41994a2afe78726
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 20:59:24 +0800

    Add files via upload

---
+++ BankAccountVersion2/BankAccountVersion2/BankAccount.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountVersion2
+{
+    internal class BankAccount
+    {
+        private double interestRate;
+        private string owner;
+        private decimal balance;
+        private long accountNumber;
+
+        private static long nextAccountNumber = 0;
+        private static ArrayList accounts = new ArrayList();
+
+        public BankAccount(string owner)
+        {
+            nextAccountNumber++;
+            accounts.Add(this);
+            this.accountNumber = nextAccountNumber;
+            this.interestRate = 0.0;
+            this.owner = owner;
+            this.balance = 0.0M;
+        }
+
+        public BankAccount(string owner, double interestRate)
+        {
+            nextAccountNumber++;
+            accounts.Add(this);
+            this.accountNumber = nextAccountNumber;
+            this.interestRate = interestRate;
+            this.owner = owner;
+            this.balance = 0.0M;
+        }
+
+        public static long NumberOfAccounts()
+        {
+            return nextAccountNumber;
+        }
+
+        public static BankAccount GetAccount(long accountNumber)
+        {
+            if(accountNumber > nextAccountNumber) return null;
+            foreach(BankAccount account in accounts)
+                if(account.accountNumber == accountNumber)
+                    return account;
+            return null;
+        }
+
+        public void Deposit(decimal desposit)
+        {
+            this.balance += desposit;
+        }
+
+        public void AddInterests()
+        {
+            this.balance = balance + (this.balance * (decimal)this.interestRate);
+        }
+
+        public override string ToString()
+        {
+            return owner + "'s account, no. " + accountNumber + " holds " +
+            + balance + " pesos.";
+        }
+
+    }
+}
---
+++ BankAccountVersion2/BankAccountVersion2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BankAccountVersion2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BankAccount a1 = new BankAccount("Kurt", 0.02),
+                        a2 = new BankAccount("Bent", 0.03),
+                        a3 = new BankAccount("Thomas", 0.02);
+
+            a1.Deposit(100.0M);
+            a2.Deposit(1000.0M); a2.AddInterests();
+            a3.Deposit(3000.0M); a3.AddInterests();
+
+            Console.WriteLine("Please enter the account number: ");
+            long accN = long.Parse(Console.ReadLine());
+
+            BankAccount a = BankAccount.GetAccount(accN);
+            if (a != null)
+                Console.WriteLine(a);
+            else
+                Console.WriteLine("Cannot find account {0}", accN);
+
+            //Program t = new Program();
+            //t.Test();
+        }
+
+
+        // <--  AN OBJECT REFERENCE IS REQUIRED FOR THE NONSTATIC FIELD -->
+
+
+        //BankAccount a1 = new BankAccount("Kurt", 0.02),
+        //                a2 = new BankAccount("Bent", 0.03),
+        //                a3 = new BankAccount("Thomas", 0.02);
+
+        //public void Test()
+        //{
+        //    a1.Deposit(100.0M);
+        //    a2.Deposit(1000.0M); a2.AddInterests();
+        //    a3.Deposit(3000.0M); a3.AddInterests();
+
+        //    Console.WriteLine("Please enter the account number: ");
+        //    long accN = long.Parse(Console.ReadLine());
+
+        //    BankAccount a = BankAccount.GetAccount(accN);
+        //    if (a != null)
+        //        Console.WriteLine(a);
+        //    else
+        //        Console.WriteLine("Cannot find account {0}", accN);
+        //}
+    }
+    
+}
