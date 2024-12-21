7dcefc17142aeea135ddb33ee48153e5ddb6cb69
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:14:38 +0800

    Add files via upload

---
+++ Private Visibility and Inheritance/Private Visibility and Inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Visibility_and_Inheritance
+{
+
+    public class A
+    {
+        private int i = 7;
+
+        protected int F(int j) { return i + j; }
+    }
+
+
+    public class B : A
+    {
+        public void G()
+        {
+            // Console.WriteLine($"i: {i}"); // unaccessible instance variable
+            Console.WriteLine($"F(5): {F(5)}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            b.G();
+        }
+    }
+}
