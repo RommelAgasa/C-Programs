b32c6053ecdf0e8dc3d71f6d39d331e5810db208
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:13:33 +0800

    Add files via upload

---
+++ Passing Structs As Value Parameters/Passing Structs As Value Parameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passing_Structs_As_Value_Parameters
+{
+    internal class Program
+    {
+
+        public struct A
+        {
+            public int a;
+            public int b;
+
+            public override string ToString()
+            {
+                return "a = " + a + ", b = " + b; 
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            A a = new A();
+
+            Console.WriteLine(a);
+            change(a);
+            Console.WriteLine(a);
+        }
+
+        public static void change(A c)
+        {
+            c.a = 1;
+            c.b = 2;
+        }
+    }
+}
