e37ac06452abfeb8899f53d0bd0ff68efe918f79
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:12:27 +0800

    Add files via upload

---
+++ Nested Object Initializer/Nested Object Initializer/BankAcoount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Object_Initializer
+{
+    internal class BankAcoount
+    {
+        public BankAcoount()
+        {
+            this.Balance = 0;
+            this.Owner = new Person();
+        }
+
+        public Person Owner { get; set; }
+        public decimal Balance { get; set; }
+
+        public override string ToString()
+        {
+            return Owner + "'s account holds " + Balance + " kroner";
+        }
+    }
+}
---
+++ Nested Object Initializer/Nested Object Initializer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested_Object_Initializer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcoount ba1 = new BankAcoount
+            {
+                Owner = { FirstName = "James", LastName = "Madsen" },
+                Balance = 250
+            },
+
+            ba2 = new BankAcoount
+            {
+                Owner = { FirstName = "Bill", LastName = "Jensen" },
+                Balance = 500
+            };
+
+            Console.WriteLine(ba1);
+            Console.WriteLine(ba2);
+
+        }
+    }
+}
