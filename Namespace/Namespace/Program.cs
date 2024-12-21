07453040a9f7d8380c9b748832e709b77d00cddd
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:11:55 +0800

    Add files via upload

---
+++ Namespace/Namespace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace N1
+{
+    public class C1 { };
+
+
+}
+
+namespace Namespace
+{
+    internal class C2 { };
+    public class C3 { };
+
+    namespace N3{
+        public class C4 {
+            C2 v;
+        }
+    }
+}
