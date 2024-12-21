9312035cc291c42cdae0e8d0db37056cc1ac2208
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:07:41 +0800

    Add files via upload

---
+++ FactoryMethod/FactoryMethod/Point_Better_Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Point_Better_Solution
+    {
+        public enum PointRepresentation { Polar, Rectangular }
+
+        private double r, a; // polar data repr: radius, angle
+
+        //Construct a point with polar coordinates
+        public Point_Better_Solution(double r, double a)
+        {
+            this.r = r;
+            this.a = a;
+        }
+
+        public static Point_Better_Solution MakePolarPoint(double r, double a)
+        {
+            return new Point_Better_Solution(r, a);
+        }
+
+        public static Point_Better_Solution MakeRectanglePoint(double x, double y)
+        {
+            return new Point_Better_Solution(RadiusGivenXy(x, y), AngleGivenXy(x, y));
+        }
+
+        private static double RadiusGivenXy(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        private static double AngleGivenXy(double x, double y)
+        {
+            return Math.Atan2(x, y);
+        }
+    }
+}
---
+++ FactoryMethod/FactoryMethod/Point_Clumsy_Attempt.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Point_Clumsy_Attempt
+    {
+        public enum PointRepresentation { Polar, Rectangular}
+        private double r, a; // polar data repr: radius, angle
+
+        //Construct a point with polar coordinates
+        public Point_Clumsy_Attempt(double r, double a)
+        {
+            this.r = r;
+            this.a = a;
+        }
+
+        // Construct a point, the representation of whic depends
+        // on the third parameter
+        public Point_Clumsy_Attempt(double par1, double par2, PointRepresentation pr)
+        {
+            
+            if(pr == PointRepresentation.Polar)
+            {
+                r = par1;
+                a = par2;
+            }
+            else
+            {
+                r = RadiusGivenXy(par1, par2);
+                a = AngleGivenXy(par1, par2);
+            }
+        }
+
+        private static double RadiusGivenXy(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        private static double AngleGivenXy(double x, double y)
+        {
+            return Math.Atan2(x, y);
+        }
+
+
+
+    }
+}
