8ccdd353673fcbb470f761341c1773251707ad1e
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:04:44 +0800

    Add files via upload

---
+++ Events Demo/Events Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Events_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die d1 = new Die();
+
+            // we add an anonymous delegate to 
+            // d1.twoSixesInARow, which reports the two sixes on the console.
+            d1.twoSixesInARow += delegate (string mes) { Console.WriteLine(mes); };
+
+            for(int i = 1; i < 5; i++)
+            {
+                d1.Toss();
+                Console.WriteLine($"{i}: {d1.NumberOfEyes}");
+            }
+        }
+    }
+}
