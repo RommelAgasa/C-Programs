d1721e1463c796fdc54a00f6e034b4c7f99ff891
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:04:04 +0800

    Add files via upload

---
+++ Event Demo 1/Event Demo 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Demo_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "Video 1" };
+            var videoEncoder = new VideoEncoder();
+            var mailService = new MailService();
+
+            // += : register a handler for that event
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded; // reference to a method
+            videoEncoder.Encode(video);
+        }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, EventArgs args)
+        {
+            Console.WriteLine("MailService: Sending an email....");
+        }
+    }
+}
