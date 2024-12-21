348c4dcab97e39b4cb294ce6cd763c2f670bd96d
_DevNote <143490735+RommelAgasa@users.noreply.github.com>
2024-12-21 21:15:13 +0800

    Add files via upload

---
+++ PropertiesDemo/PropertiesDemo/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    internal class BankAccount
+    {
+        private string owner;
+        private decimal[] contributions;
+        private int nextContributions;
+
+        public BankAccount(string owner, decimal balance)
+        {
+            this.owner = owner;
+            this.contributions = new decimal[100];
+            this.contributions[0] = balance;
+            this.nextContributions = 1;
+        }
+
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal results = 0.0m;
+                foreach (decimal ctr in contributions)
+                    results += ctr;
+                return results;
+            }
+        }
+
+        public void Deposits(Decimal amount)
+        {
+            contributions[nextContributions] += amount;
+            nextContributions++;
+        }
+
+        public void Withdraws(Decimal amount)
+        {
+            contributions[nextContributions] -= amount;
+            nextContributions++;
+        }
+
+        public override string ToString()
+        {
+            return owner + "'s account holds " + Balance + "pesos.";
+        }
+
+
+    }
+}
---
+++ PropertiesDemo/PropertiesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount ba = new BankAccount("Peter", 1000);
+            Console.WriteLine(ba);
+
+            ba.Deposits(100);
+            Console.WriteLine("Balance: {0}", ba.Balance);
+
+            ba.Withdraws(300);
+            Console.WriteLine("Balance: {0}", ba.Balance);
+
+            ba.Deposits(100);
+            Console.WriteLine("Balance: {0}", ba.Balance);
+
+        }
+    }
+}
